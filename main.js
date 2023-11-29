9c5cde8e9eb619a77549e34dce05cb496cdd6aeb
Juan <118556304+Juan7w7@users.noreply.github.com>
2023-11-29 13:31:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const btnSi = document.querySelector('#btnSi');
 
 btnSi.addEventListener('click',function () {
-    alert('Sabía que ibas a atrapar al gato 7w7')
+    alert('Sabía que ibas a atrapar al gato, paso por ti el sabado 7w7.')
 });
 
 const btnNo = document.querySelector('#btnNo');
